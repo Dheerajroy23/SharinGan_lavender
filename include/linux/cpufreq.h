f030a2c8420b3b2fcb7bb9ffadad8e957d8ea48a
evirakernel <evirakernel@gmail.com>
2019-05-21 14:26:50 +0200

    added alucardsched zen and enable on defconfig

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -537,6 +537,7 @@ extern struct cpufreq_governor cpufreq_gov_sched;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHEDUTIL)
 extern struct cpufreq_governor cpufreq_gov_schedutil;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_schedutil)
+
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
